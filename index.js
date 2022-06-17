ec176fb1c2bfb5cda7a37d37928f0af6c8d8f8d9
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-17 17:47:10 +0300

    commit tree 1820395344608a774617fe4eba927f2255b789ac

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+   return customerName.toUpperCase();
+}
+console.log(upperCaseCustomerName())
+
+function setBestCustomer() {
+    var bestCustomer = 'not bob';
+}
+
+function  overwriteBestCustomer() {
+     bestCustomer = "Is Kelv"
+}
